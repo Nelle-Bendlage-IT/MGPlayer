4a10ab7c234f2d64caf74477db0217bbd2ffc473
jnelle <jimmy@nelle.sh>
2024-07-21 16:18:21 +0200

    feat: refactor clip management and authentication handling
    - Change `time` field type from `Instant` to `Long` in `Clip` model
    - Add formatting methods for duration, release date, and vertical image in `Clip` model
    - Add `Serializable` annotation to `ProgressResponse` and `ProgressTrace`
    - Replace `lastAccessed` field type from `Instant` to `Long` in `ProgressClip`
    - Modify the `getRecentlyWatched` method to include authentication parameters and update API endpoint call
    - Add `basicAuth` method in the API service call for user authentication
    - Include new data class `ClipData` with properties for progress, clip, file, and clip ID in the `CommonViewModel`
    - Implement getters and setters for `progress`, `clip`, `file`, and `clipID` in `CommonPostModel` ensuring updates reflect in UI state management
    
    Signed-off-by: jnelle <jimmy@nelle.sh>

--- mgtvApi/src/commonMain/kotlin/com/mgtvapi/viewModel/CommonViewModel.kt
+++ mgtvApi/src/commonMain/kotlin/com/mgtvapi/viewModel/CommonViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.touchlab.kermit.Logger
 import com.mgtv.shared_core.core.ViewState
+import com.mgtvapi.api.model.Clip
+import com.mgtvapi.api.model.File
+import com.mgtvapi.api.model.Progress
 import com.mgtvapi.api.model.WatchResponse
 import com.mgtvapi.api.repository.MGTVApiRepository
 import kotlinx.coroutines.Dispatchers
@@ -14,11 +17,41 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
+data class ClipData(
+    val progress: Progress? = null,
+    val clip: Clip? = null,
+    val file: File? = null,
+    val clipID: String = ""
+)
 
 class CommonViewModel(private val repo: MGTVApiRepository) : KoinComponent, ViewModel() {
     private var _clipData = MutableStateFlow<ViewState<WatchResponse>>(ViewState.Empty)
     val clipData: StateFlow<ViewState<WatchResponse>> = _clipData.asStateFlow()
+    private var _selectedClip: ClipData = ClipData()
+    var progress: Progress?
+        get() = _selectedClip.progress
+        set(value) {
+            _selectedClip = _selectedClip.copy(progress = value)
+        }
 
+    var clip: Clip?
+        get() = _selectedClip.clip
+        set(value) {
+            _selectedClip = _selectedClip.copy(clip = value)
+        }
+
+    var file: File?
+        get() = _selectedClip.file
+        set(value) {
+            _selectedClip = _selectedClip.copy(file = value)
+        }
+
+    var clipID: String
+        get() = _selectedClip.clipID
+        set(value) {
+            _selectedClip = _selectedClip.copy(clipID = value)
+        }
+    
     fun getClipFiles(clipId: String) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
