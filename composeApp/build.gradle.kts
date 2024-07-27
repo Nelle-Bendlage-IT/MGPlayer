bfd7f85b51ba8a76a5136026d0e0ffffec006d9e
jnelle <jimmy@nelle.sh>
2024-07-27 13:42:46 +0200

    build: update project build configurations and dependencies
    - Update the `compose.compiler` plugin reference to `build.gradle.kts`
    - Reorder plugin applications with addition of `kotlinMultiplatform` in `composeApp` project
    - Update the Android Gradle Plugin and androidx libraries versions in the `libs.versions.toml`
    - Include new implementations of Compose UI tooling and previews across multiple `build.gradle.kts` files
    - Remove Cocoapods related configuration from the `mgtvApi` project
    - Reorganize project module dependencies in the `mgplayertv` build file
    
    Signed-off-by: jnelle <jimmy@nelle.sh>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
-    alias(libs.plugins.compose.compiler) // apply false
+    alias(libs.plugins.compose.compiler)
     id("kotlin-parcelize") // add this
     id("kotlin-kapt") // add this
     kotlin("plugin.serialization") version "2.0.0"
@@ -38,7 +38,7 @@ kotlin {
     sourceSets {
 
         androidMain.dependencies {
-            implementation(libs.compose.ui.tooling.preview)
+            implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.androidx.media3.ui)
             implementation(libs.androidx.media3.common)
@@ -46,6 +46,7 @@ kotlin {
             implementation(libs.androidx.media3.session)
         }
         commonMain.dependencies {
+            implementation(compose.components.uiToolingPreview)
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.materialIconsExtended)
@@ -127,6 +128,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     dependencies {
-        debugImplementation(libs.compose.ui.tooling)
+        debugImplementation(compose.uiTooling)
     }
 }
